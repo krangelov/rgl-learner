1f3092adaad1e1d97244a71bd66f8753a6266604
EkaterinaVoloshina <vokat@mail.ru>
2024-09-12 17:35:44 +0200

    modified version of lemma tree

--- smart_paradigms/lemma_tree.py
+++ smart_paradigms/lemma_tree.py
@@ -2,64 +2,140 @@ import sys
 import pickle
 from collections import defaultdict, Counter
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import train_test_split
 import numpy as np
 from smart_paradigms.utils import *
 from scipy.stats import entropy
+import re
+
+def form_token(stem, assignments, labels, forms):
+    token_dict = {}
+    if assignments:
+        base_dict = {}
+        for assignment in assignments:
+            base, ass = assignment.split(" = ")
+            base_dict[base] = parse_ass(stem, ass)
+    else:
+        base_dict = {"base_1": stem}
+
+
+    for label, form in zip(labels, forms):
+        if form != "nonExist":
+            for base, morpheme in base_dict.items():
+                form = form.replace(base, morpheme)
+            form = re.sub(r"base_\d", r"", form)
+            token_dict[label] = form.replace("+", "").replace('"', "")
+        else:
+            token_dict[label] = "-"
+    return token_dict
+
+
+def format_unimorph(tokens):
+    unimorph = []
+    for (pos, lemma, forms) in tokens:
+        for tag, form in forms.items():
+            um_tag = ";".join([pos,] + [gf2unimorph[t] for t in tag.split(";") if t in gf2unimorph])
+            unimorph.append("\t".join([lemma, form, um_tag]))
+    return "\n".join(unimorph)
 
 class LemmaTree:
     def __init__(self,
-                 lemmas,
-                 max_depth=3,
-                 min_samples_leaf=3,
-                 stopping=None):
-        self.lemmas = lemmas
+                 pos: str,
+                 forms: list,
+                 max_depth: int = 3,
+                 min_samples_leaf: int = 3,
+                 min_examples: int = 1,
+                 how: str = "suffix",
+                 stopping: str = None,
+                 split: bool = False):
+
+        self.pos = pos
+        self.forms = forms
+        if split:
+            lemmas = [((lemma[0].split("_")[0], tag), lemma[1]) for tag, paradigm in enumerate(forms) for lemma in
+                      paradigm.tables]
+            train, test = train_test_split(lemmas, shuffle=True)
+            labels, _ = list(zip(*train))
+            count_labels = Counter(list(zip(*labels))[1])
+            self.train = [x for x in labels if count_labels[x[1]] >= min_examples]
+
+        else:
+            train = [((lemma[0].split("_")[0], tag), lemma[1]) for tag, paradigm in enumerate(forms) for lemma in
+                       paradigm.tables if len(paradigm.tables) >= min_examples]
+            test = train
+            self.train, _ = list(zip(*train))
+
+        self.test, self.tokens = list(zip(*test))
+
         if stopping == "max_length":
-            self.max_depth = max([len(x[0]) for x in self.lemmas]) + 1
+            self.max_depth = max([len(x[0]) for x in self.train]) + 1
         else:
             self.max_depth = max_depth
 
         self.min_samples_leaf = min_samples_leaf
         self.rules = defaultdict()
         self.regex = defaultdict()
+        self.how = how
+
+    def update_rules(self, label, value):
+        found = False
+        for i in range(len(label)):
+            if self.how == "suffix":
+                prev = label[i:]
+            else:
+                prev = label[:-i]
+            if prev in self.rules and self.rules[prev] == value:
+                found = True
+                break
+        if not found:
+            self.rules[label] = value
 
     def calculate_entropy(self, labels):
         label2entropy = defaultdict(list)
         for label, tags in labels.items():
             lemmas, classes = list(zip(*tags))
             classes = Counter(classes)
-            classes = [one_class/sum(classes.values()) for one_class in classes.values()]
-            label2entropy[label] = entropy(classes)
+            class_dist = {class_name: one_class/sum(classes.values()) for class_name, one_class in classes.items()}
+            label2entropy[label] = (entropy(list(class_dist.values())), class_dist)
         return label2entropy
-
-    def check_sister_nodes(self, label, entropy, labels):
-        non_zeros = np.count_nonzero(np.asarray(list(entropy.values())))
-        if non_zeros == 1:
-            other_label = set([l[1] for other_label, values in labels.items() for l in values if other_label != label])
-            cur_labels = [l[1] for l in labels[label]]
-            if len(other_label) == 1 and next(iter(other_label)) in cur_labels:
-                return next(iter(other_label))
 
     def build_node(self, lemmas, position):
         label2classes = defaultdict(list)
         for lemma, tag in lemmas:
             if len(lemma) > position:
-                label2classes[lemma[-position:]].append((lemma, tag))
+                if self.how == "suffix":
+                    label2classes[lemma[-position:]].append((lemma, tag))
+                else:
+                    label2classes[lemma[:position]].append((lemma, tag))
+
         return self.calculate_entropy(label2classes), label2classes
+
+
 
     def build_tree(self, lemmas, position=1):
         entropy_scores, label2classes = self.build_node(lemmas, position)
         position += 1
-        for label, entropy_score in entropy_scores.items():
+        for label, (entropy_score, class_dist) in entropy_scores.items():
             if entropy_score == 0:
-                self.rules[label] = label2classes[label][0][1]
+                self.update_rules(label, label2classes[label][0][1])
             else:
-                class_tag = self.check_sister_nodes(label, entropy_scores, label2classes)
-                if class_tag:
-                    self.rules[label] = class_tag
-                elif position <= self.max_depth and len(label2classes[label]) >= self.min_samples_leaf:
-                    self.build_tree(label2classes[label], position)
+                maxval = max(class_dist.values())
+                res = list(filter(lambda x: class_dist[x] == maxval, class_dist))
+                if len(res) == 1:
+                    self.update_rules(label, res[0])
                 else:
-                    self.rules[label] = next(iter(Counter([l[1] for l in label2classes[label]])))
+                    classes = Counter([x[1] for x in lemmas if x[1] in res])
+                    maxval = max(classes.values())
+
+                    for i in res:
+                        if classes[i] == maxval:
+                            self.update_rules(label, i)
+                            break
+                if position <= self.max_depth and len(label2classes[label]) >= self.min_samples_leaf:
+                    self.build_tree(label2classes[label], position)
+
+
+
 
     def build_regex(self, rule):
         regex = defaultdict(list)
@@ -84,55 +160,90 @@ class LemmaTree:
         for tag, rule in class2labels.items():
             self.regex[tag] = self.build_regex(rule)
 
-    def compute_metrics(self):
-        accuracy = 0
+    def evaluate(self):
         y_true = []
         y_pred = []
-        for lemma, tag in self.lemmas:
+        self.rules = dict(sorted(self.rules.items(), key=lambda x: len(x[0]), reverse=True))
+
+        for lemma, tag in self.test:
             y_true.append(tag)
             pred = -1
-            for rule, pred_tag in self.rules.items():
-                if lemma.endswith(rule):
-                    pred = pred_tag
-                    break
+            for l in range(self.max_depth, 0, -1):
+                for rule, pred_tag in self.rules.items():
+                    if len(rule) == l and ((self.how == "suffix" and lemma.endswith(rule)) or (self.how == "prefix" and lemma.startswith(rule))):
+                        pred = pred_tag
+                        break
+                else:
+                    continue
+                break
+          #  print(lemma, pred)
             y_pred.append(pred)
-        return {"accuracy": accuracy_score(y_true, y_pred),
+
+        coverage = []
+        preds = []
+
+        for (lemma, tag), tokens, pred in zip(self.test, self.tokens, y_pred):
+            table = self.forms[pred]
+
+            true_labels = reverse_dict(self.forms[tag].typ.linearize())
+            pred_labels = reverse_dict(table.typ.linearize())
+
+            true_forms = dict(zip(true_labels, tokens))
+
+            tokens = [c for c in tokens if c != "-"]
+            if tokens:
+                base = tokens[0]
+            else:
+                base = lemma
+            pred_forms = form_token(base, table.assignments, pred_labels, table.forms)
+
+            preds.append((self.pos, lemma, pred_forms))
+
+            coverage.extend(coverage_score(true_forms, pred_forms))
+
+        return preds, {"accuracy": accuracy_score(y_true, y_pred),
                 "f1": f1_score(y_true, y_pred, average="micro"),
                 "precision": precision_score(y_true, y_pred, average="micro"),
-                "recall": recall_score(y_true, y_pred, average="micro")}
+                "recall": recall_score(y_true, y_pred, average="micro"),
+                "coverage": sum(coverage) / len(coverage)}
 
-def write_gf_code(rules, pos_tag, forms2bases):
-    gf_code = f"""formBasedSelection{pos_tag} : Str => {pos_tag}Class\n= \\lemma -> case lemma of {{\n"""
-    for rule, class_tag in rules.items():
+def write_gf_code(lang, rules, pos_tag):
+    gf_code = f"""formBasedSelection{pos_tag} : Str -> {pos_tag}\n= \\lemma -> case lemma of {{\n"""
+    rules = sorted(rules.items(), key=lambda x: len(x[0]), reverse=True)
+    for rule, class_tag in rules:
         tag = str(class_tag+1).zfill(3)
-        gf_code += f"""\t\t_ + "{rule}" -> mk{pos_tag}{tag} lemma"""
-        if forms2bases[class_tag] > 1:
-            gf_code += ' ""' * (forms2bases[class_tag]-1)
-        gf_code += ";\n"
+        gf_code += f"""\t\t_ + "{rule}" => mk{pos_tag}{tag} lemma;\n"""
     gf_code += "} ;\n"
-    gf_code = gf_code.replace(";\n}", "\n}")
+    gf_code = gf_code.replace(";\n}", "\t\n}")
     return gf_code
 
-def guess_by_lemma(lang):
-    print("Reading data..")
-    langcode, tables = read_data(lang)
-    code = ""
-    for pos, forms in tables.items():
-        if len(forms) > 1:
-            print(f"=={pos}==")
-            lemma2class = [(lemma[0].split("_")[0], tag) for tag, paradigm in enumerate(forms) for lemma in paradigm.tables]
-            forms2bases = {n: len(table.var_insts[0]) for n, table in enumerate(forms)}
-            lemma_tree = LemmaTree(lemma2class)
-            lemma_tree.build_tree(lemma2class)
-            lemma_tree.linearize_tree()
-            #print(lemma_tree.regex)
-            scores = lemma_tree.compute_metrics()
-            print(scores)
+def guess_by_lemma(lang, multiple_forms=False):
+        print("Reading data..")
+        langcode, tables = read_data(lang)
+        tokens = []
+        code = ""
+        for pos, forms in tables.items():
+            if len(forms) > 1:
+                print(f"=={pos}==")
+                if multiple_forms:
+                    continue
+                else:
+                    tree = LemmaTree(pos, forms, how="suffix", max_depth=3, min_examples=2, min_samples_leaf=2)
+                    tree.build_tree(tree.train)
+                    tree.linearize_tree()
 
-            code += write_gf_code(lemma_tree.rules, pos, forms2bases)
+                    preds, scores = tree.evaluate()
 
-    with open(f"Inflection{langcode}.gf", "w") as f:
-        f.write(f"resource Inflection{langcode} = open Prelude, Res{langcode}, Paradigms{langcode} in {{\noper\n")
-        f.write(code)
-        f.write("}")
+                tokens.extend(preds)
+                print(scores)
 
+                code += write_gf_code(langcode, tree.rules, pos)
+
+        with open(f"Inflection{langcode}.gf", "w") as f:
+            f.write(f"resource Inflection{langcode} = open Prelude, Res{langcode}, Paradigms{langcode} in {{\noper\n")
+            f.write(code)
+            f.write("}")
+
+        unimorph_code = format_unimorph(tokens)
+        with open(f"unimorph_{langcode}.tsv", "w") as f:
+            f.write(unimorph_code)
--- smart_paradigms/utils.py
+++ smart_paradigms/utils.py
@@ -6,25 +6,288 @@ import itertools
 import pandas as pd
 import pickle
 
+cat2tag = {
+  'N': 'Noun',
+  'V':'Verb',
+  'A': 'Adj',
+   'PN': 'Name',
+  'Pron': 'Pron',
+  'Adv': 'Adv',
+  'Det': 'Det',
+  'Prep': 'Prep',
+}
+
+
+gf2unimorph = {'Indicative': 'IND',
+ 'Admirative': 'ADM',
+ 'Non_Purposive': 'AUNPRP',
+ 'Purposive': 'AUPRP',
+ 'Conditional': 'COND',
+ 'Debitive': 'DEB',
+ 'Deductive': 'DED',
+ 'Imperative_Jussive': 'IMP',
+ 'Intentive': 'INTEN',
+ 'Irrealis': 'IRR',
+ 'Likely': 'LKLY',
+ 'Obligative': 'OBLIG',
+ 'Optative_Desiderative': 'OPT',
+ 'Permissive': 'PERM',
+ 'Potential': 'POT',
+ 'General Purposive': 'PURP',
+ 'Realis': 'REAL',
+ 'Subjunctive': 'SBJV',
+ 'Simulative': 'SIM',
+ '1day': '1DAY',
+ 'Fut': 'FUT',
+ 'Hodiernal': 'HOD',
+ 'Immediate': 'IMMED',
+ 'Pres': 'PRS',
+ 'Past': 'PST',
+ 'Recent': 'RCT',
+ 'Remote': 'REMT',
+ 'Alien': 'ALN',
+ 'Inalien': 'NALN',
+ 'Poss1Du': 'PSS1D',
+ 'Poss1DuExcl': 'PSS1DE',
+ 'Poss1DucIncl': 'PSS1DI',
+ 'Poss1Pl': 'PSS1P',
+ 'Poss1PlExcl': 'PSS1PE',
+ 'Poss1PlIncl': 'PSS1PI',
+ 'Poss1Sg': 'PSS1S',
+ 'Poss2Du': 'PSS2D',
+ 'Poss2DuFem': 'PSS2DF',
+ 'Poss2DuMasc': 'PSS2DM',
+ 'Poss2PL': 'PSS2P',
+ 'Poss2PlFem': 'PSS2PF',
+ 'Poss2PlMasc': 'PSS2PM',
+ 'Poss2Sg': 'PSS2S',
+ 'Poss2SgFem': 'PSS2SF',
+ 'Poss2SgForm': 'PSS2SFORM',
+ 'Poss2SgInfm': 'PSS2SINFM',
+ 'Poss2SgMasc': 'PSS3SM',
+ 'Poss3Du': 'PSS3D',
+ 'Poss3DuFem': 'PSS3DF',
+ 'Poss3DuMasc': 'PSS3DM',
+ 'Poss3Pl': 'PSS3P',
+ 'Poss3PlFem': 'PSS3PF',
+ 'Poss3PlMasc': 'PSS3PM',
+ 'Poss3Sg': 'PSS3S',
+ 'Poss3Ssg': 'PSS3SG',
+ 'Poss3SgFem': 'PSS3SF',
+ 'Possessed': 'PSSD',
+ 'Accomplishment': 'ACCMP',
+ 'Achievement': 'ACH',
+ 'Activity': 'ACTY',
+ 'Atelic': 'ATEL',
+ 'Durative': 'DUR',
+ 'Dynamic': 'DYN',
+ 'Punctual': 'PCT',
+ 'Semelfactive': 'SEMEL',
+ 'Stative': 'STAT',
+ 'Telic': 'TEL',
+ '3.sg Object (from feature template)': 'ARGAC3S',
+ 'Habitual': 'HAB',
+ 'Imperfective': 'IPFV',
+ 'Iterative': 'ITER',
+ 'Perfective': 'PFV',
+ 'Perfect': 'PRF',
+ 'Progressive': 'PROG',
+ 'Prospective': 'PROSP',
+ 'Indef': 'INDF',
+ 'Def': 'DEF',
+ 'Non_Spec': 'NSPEC',
+ 'Spec': 'SPEC',
+ 'Nom': 'NOM',
+ 'Acc': 'ACC',
+ 'Dat': 'DAT',
+ 'Gen': 'GEN',
+ 'Voc': 'VOC',
+ 'Part': 'PRT',
+ 'Iness': 'IN',
+ 'Ablat': 'ABL',
+ 'Ess': 'ESS',
+ 'Transl': 'PROL',
+ 'Instr': 'INST',
+ 'Comit': 'COM',
+ 'Loc': 'LOC',
+ 'SNom': 'NOMS',
+ 'On': 'ON',
+ 'Onhr': 'ONHR',
+ 'Onvr': 'ONVR',
+ 'Post': 'POST',
+ 'Privative': 'PRIV',
+ 'Proprietive': 'PROPR',
+ 'Abs': 'ABS',
+ 'Allat': 'ALL',
+ 'Ante': 'ANTE',
+ 'Apprx': 'APPRX',
+ 'Apud': 'APUD',
+ 'At': 'AT',
+ 'Aver': 'AVR',
+ 'Benef': 'BEN',
+ 'Essmod': 'BYWAY',
+ 'Circ': 'CIRC',
+ 'Compar': 'COMPV',
+ 'Equative': 'EQT',
+ 'Erg': 'ERG',
+ 'Formal': 'FORM',
+ 'Inter': 'INTER',
+ 'Purp': 'PRP',
+ 'Rel': 'REL',
+ 'Rem': 'REM',
+ 'Sub': 'SUB',
+ 'Term': 'TERM',
+ 'Vers': 'VERS',
+ 'Absolute': 'AB',
+ 'Comparative': 'CMPR',
+ 'Relative': 'RL',
+ 'Superlative': 'SPRL',
+ 'Proximal': 'PROX',
+ 'Above': 'ABV',
+ 'Below': 'BEL',
+ 'Even': 'EVEN',
+ 'Medial': 'MED',
+ 'No Reference Point, Distal': 'NOREF',
+ 'Invisible': 'NVIS',
+ 'Phoric, situated in discourse': 'PHOR',
+ 'First Person Reference Point': 'REF1',
+ 'Second Person Reference Point': 'REF2',
+ 'Visible': 'VIS',
+ 'Assumed': 'ASSUM',
+ 'Auditory': 'AUD',
+ 'Direct': 'DIR',
+ 'Firsthand': 'FH',
+ 'Hearsay': 'HRSY',
+ 'Inferred': 'INFER',
+ 'Non_firsthand': 'NFH',
+ 'Non_visual sensory': 'NVSEN',
+ 'Quotative': 'QUOT',
+ 'Reported': 'RPRT',
+ 'Sensory': 'SEN',
+ 'Finite': 'FIN',
+ 'Nonfinite': 'NFIN',
+ 'NClass': 'BANTU1_23',
+ 'Masc': 'MASC',
+ 'Fem': 'FEM',
+ 'Neutr': 'NEUT',
+ 'NClasses': 'NAKH1-8',
+ 'Focus': 'FOC',
+ 'Topic': 'TOP',
+ 'Declarative': 'DECL',
+ 'Interrogative': 'INT',
+ 'Excl': 'EXCL',
+ 'Incl': 'INCL',
+ 'Obviative': 'OBV',
+ 'Proximate': 'PRX',
+ 'Affirm': 'POS',
+ 'Neg': 'NEG',
+ 'CN_R_MN': 'CN_R_MN',
+ 'DS': 'DS',
+ 'DSAdv': 'DSADV',
+ 'Logophoric': 'LOG',
+ 'Open_Reference': 'OR',
+ 'SeqMA': 'SEQMA',
+ 'SimMA': 'SIMMA',
+ 'SS': 'SS',
+ 'SS Adverbial': 'SSADV',
+ 'Applicative': 'APPL',
+ 'Causative': 'CAUS',
+ 'Imprs': 'IMPRS',
+ 'Intransitive': 'INTR',
+ 'Reciprocal': 'RECP',
+ 'Reflexive': 'REFL',
+ 'Transitive': 'TR',
+ 'AcFoc': 'ACFOC',
+ 'Active': 'ACT',
+ 'AgFoc': 'AGFOC',
+
+ 'Antipassive': 'ANTIP',
+ 'BFocus': 'BFOC',
+ 'CFocus': 'CFOC',
+ 'IFocus': 'IFOC',
+ 'Inverse': 'INVN',
+ 'LFocus': 'LFOC',
+ 'Middle': 'MID',
+ 'Passive': 'PASS',
+ 'PFocus': 'PFOC',
+ 'P0': '0',
+ 'P1': '1',
+ 'P2': '2',
+ 'P2_Informal': '2I',
+ 'P2_Formal': '2F',
+ 'P3': '3',
+ 'P4': '4',
+ 'High status, elevated': 'STELEV',
+ 'High status, supreme': 'STSUPR',
+ 'Avoid': 'AVOID',
+ 'Colloquial': 'COL',
+ 'Elev': 'ELEV',
+ 'Forreg': 'FOREG',
+ 'High_status': 'HIGH',
+ 'Humbl': 'HUMB',
+ 'Informal': 'INFM',
+ 'Literary': 'LIT',
+ 'Low status': 'LOW',
+ 'Polite': 'POL',
+ 'Animate': 'ANIM',
+ 'Human': 'HUM',
+ 'Inanimate': 'INAN',
+ 'NonHuman': 'NHUM',
+ 'GrPaucal': 'GPAUC',
+ 'GrPlural': 'GRPL',
+ 'Paucal': 'PAUC',
+ 'Sg': 'SG',
+ 'Dl': 'DU',
+ 'Tr': 'TRI',
+ 'Pl': 'PL'}
 
 def read_data(lang):
     with open(f"data/{lang}/paradigms.pickle", "rb") as f:
         langcode, tables = pickle.load(f)
-    return tables
+    return langcode, tables
+
+def parse_ass(word, ass):
+    ass = ass.split()
+    if ass[0] == "dp":
+        if ass[2].strip("(") == "tk":
+            num1 = int(ass[3])
+            word = word[:-num1]
+        num = int(ass[1])
+        return word[-num:]
+    elif ass[0] == "tk":
+        num = int(ass[1])
+        return word[:-num]
+    elif ass[0] == "drop":
+        if ass[2].strip("(") == "tk":
+            num1 = int(ass[3])
+            word =  word[:-num1]
+        num = int(ass[1])
+        return word[num:]
+    else:
+        return word
+
 
 
 def form_tokens(tokens, assignments, forms, labels):
     token_list = []
+   # print(assignments)
     for token in tokens:
+        base, stem = token[0]
         token_dict = {}
+        if assignments:
+            base_dict = {}
+            for assignment in assignments:
+                base, ass = assignment.split(" = ")
+                base_dict[base] = parse_ass(stem, ass)
+        else:
+            base_dict = {"base_1": stem}
         for label, form in zip(labels, forms):
             if form != "nonExist":
-                for base, stem in token:
-                    if base == "base":
-                        form = form.replace("base_1", stem)
-                    else:
-                        form = form.replace(base, stem)
+                for base, morpheme in base_dict.items():
+                    form = form.replace(base, morpheme)
                 token_dict[label] = form.replace("+", "").replace('"', "")
+            else:
+                token_dict[label] = "nonExist"
         token_list.append(token_dict)
     return token_list
 
@@ -73,18 +336,17 @@ def prepare_data(df, feature_list, class_tags, how="suffix"):
     return feats, labels
 
 
-def coverage_score(token, forms):
-    coverage = 0
-    for form, value in token.items():
-        if forms.get(form) == value:
-            coverage += 1
-        #else:
-        #    print(forms.get(form), value)
 
-    overlap = len(forms.keys() & token.keys())
-    return (coverage/(len(token)),
-            coverage/(len(forms)),
-            coverage/overlap)
+def coverage_score(token, forms):
+    coverage = [1]
+    for form, value in token.items():
+        if value != "-":
+            if forms.get(form) == value:
+                coverage.append(1)
+            else:
+                coverage.append(0)
+                #print(forms.get(form), value)
+    return coverage
 
 def build_tree(X, y):
     tree = DecisionTreeClassifier()
