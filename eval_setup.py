e9ff0e75b642c94fb2f1e6f5b19091a4e54302e4
EkaterinaVoloshina <vokat@mail.ru>
2024-11-26 15:06:25 +0100

    fixed eval setup file

--- eval_setup.py
+++ eval_setup.py
@@ -2,14 +2,11 @@ import sys
 from evaluation import prepare_lang, run_conll
 
 
-if len(sys.argv) == 3 and sys.argv[1] == "prepare_lang":
+if len(sys.argv) == 3 and sys.argv[1] == "prepare-lang":
     prepare_lang.prepare_lang(sys.argv[2])
-elif len(sys.argv) == 4 and sys.argv[1] == "run_conll":
+elif len(sys.argv) == 4 and sys.argv[1] == "run-conll":
     run_conll.extract(sys.argv[2], sys.argv[3])
 else:
-    print(f"Synopsis: {sys.argv[0]} morpho-cats <wiktionary|unimorph> <lang-code>")
-    print(f"          {sys.argv[0]} construct-table <wiktionary|unimorph> <lang-code>")
-    print(f"          {sys.argv[0]} learn-paradigms <lang-code>")
-    print(f"          {sys.argv[0]} smart-paradigms <lang-code>")
-    print(f"          {sys.argv[0]} build-lemma-tree <lang-code>")
-    print(f"          {sys.argv[0]} smart-dictionary <lang-code>")
+    print(f"Synopsis: {sys.argv[0]} prepare-lang  <lang-code>")
+    print(f"          {sys.argv[0]} run-conll <lang-code> <level>")
+
