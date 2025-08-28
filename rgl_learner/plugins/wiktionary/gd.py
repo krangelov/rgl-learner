2b0761f31bee50aee3759687d74e0d3855ad24cc
Krasimir Angelov <kr.angelov@gmail.com>
2025-08-28 16:04:39 +0200

    covered all nouns

--- rgl_learner/plugins/wiktionary/gd.py
+++ rgl_learner/plugins/wiktionary/gd.py
@@ -1,9 +1,40 @@
 iso3 = "Gla"
 
+vars = {}
+
+def preprocess(record):
+    if record.get("pos") == "noun":
+        categories = record.get("categories",[])
+
+        found = False
+
+        def extract_gender(categories):
+            nonlocal found
+            if 'Irish masculine nouns' in categories:
+                found = True
+                return ["masculine"]
+            elif 'Irish feminine nouns' in categories:
+                found = True
+                return ["feminine"]
+            elif 'Irish neuter nouns' in categories:
+                found = True
+                return ["neuter"]
+            return []
+
+        record["tags"] = extract_gender(categories)
+        for sense in record["senses"]:
+            sense["tags"] = extract_gender(sense.get("categories",[]))
+
+        if not found:
+            record["tags"] = ["masculine"]
+
+    return True
+
 def filter_lemma(lemma, pos, table):
     if pos == "noun" and lemma == "Leòmhann":
         return True
-
+    if pos != "noun":
+        return True
     return False
 
 def patchPOS(lemma,tag,table):
@@ -21,17 +52,24 @@ def patchN(lemma,table):
             tbl["nominative"]["indefinite"][number] = feminine
     for case, forms in tbl.items():
         if type(forms) == str:
-            print(case,forms)
             tbl[case] = {"indefinite": forms, "definite": "-"}
         else:
             o = forms.pop("noSpecies",{})
             if type(o) == str:
-                tbl["nominative"]["indefinite"] = o
+                tbl["nominative"]["indefinite"]["singular"] = o
+                tbl["nominative"]["indefinite"]["plural"] = "-"
             else:
-                for tag,form in forms.pop("noSpecies",{}).items():
-                    if form != "-":
-                        tbl[case]["indefinite"] = form
+                for tag,form in o.items():
+                    if form == "no-table-tags":
+                        continue
+                    tbl[case].setdefault("indefinite",{}).setdefault(tag,form)
     noCase = tbl.pop("noCase",{})
     if noCase:
-        tbl["nominative"]["indefinite"] = noCase
+        if type(noCase) == str:
+            noCase = {"indefinite": {"singular": noCase, "plural": "-"}, "definite": {"singular": "-", "plural": "-"}}
+        tbl["nominative"] = noCase
+    tbl["nominative"]["indefinite"]["singular"] = lemma
+    tbl.setdefault("nominative",{}).setdefault("indefinite",{}).setdefault("plural","-")
+    
     table["s"] = tbl
+    table["voc"] = tbl.pop("vocative",{}).pop("definite",{"singular": "-", "plural": "-"})
